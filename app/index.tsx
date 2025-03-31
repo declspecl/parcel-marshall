c5ffe0ab674d1141c90488427005c800d14e46f6
declspecl <gavind2559@gmail.com>
2025-03-31 15:45:37 -0400

    styling setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
-export default function Index() {
-  return (
-    <View
-      style={{
+const styles = StyleSheet.create({
+    container: {
         flex: 1,
         justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+        alignItems: "center"
+    }
+});
+
+export default function Index() {
+    return (
+        <View style={styles.container}>
+            <Text>Hello, World!</Text>
+        </View>
+    );
 }
