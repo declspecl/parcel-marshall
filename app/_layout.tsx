7663eb72bf0909b760d96b0b01f2049d69eeec2f
declspecl <gavind2559@gmail.com>
2025-04-09 21:53:00 -0400

    setup nix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,21 +15,24 @@ export default function RootLayout() {
                     name="index"
                     options={{
                         tabBarLabel: "Home",
-                        tabBarIcon: ({ color, size }) => <Ionicons name="home" size={size} color={color} />
+                        tabBarIcon: ({ color, size }) => <Ionicons name="home" size={size} color={color} />,
+                        title: "ParcelMarshall: Next stop: World domination (or just Gavin’s Rust Hideout) 🦀🧠"
                     }}
                 />
                 <Tabs.Screen
                     name="route"
                     options={{
                         tabBarLabel: "Route",
-                        tabBarIcon: ({ color, size }) => <Ionicons name="map" size={size} color={color} />
+                        tabBarIcon: ({ color, size }) => <Ionicons name="map" size={size} color={color} />,
+                        title: "ParcelMarshall: Drift Mode Engaged 🏎️💨"
                     }}
                 />
                 <Tabs.Screen
                     name="settings"
                     options={{
                         tabBarLabel: "Settings",
-                        tabBarIcon: ({ color, size }) => <Ionicons name="settings" size={size} color={color} />
+                        tabBarIcon: ({ color, size }) => <Ionicons name="settings" size={size} color={color} />,
+                        title: "Settings"
                     }}
                 />
             </Tabs>
